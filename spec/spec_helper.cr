03175adbd468705b92595322f35874c28e34f3f0
Stephen von Takach <steve@place.technology>
2025-12-12 00:51:26 +1100

    chore(spec): output default servers before spec runs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -9,6 +9,10 @@ Spec.before_suite do
   ::Log.setup("*", :trace)
 end
 
+servers = DNS::Servers.from_host
+servers = DNS::Servers.fallback if servers.empty?
+puts "Default Servers: #{servers}"
+
 Spec.before_each do
   DNS.cache = DNS::Cache::HashMap.new
   DNS.default_resolver = DNS::Resolver::UDP.new
